0a7d7ab8db86026914f07de95a312cfe08b4d0ea
sourabh-kaujalgi <Sourabh.kaujalgi@gmail.com>
2026-04-14 02:43:48 +0530

    conf file change

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,19 +1,57 @@
 exports.config = {
     runner: 'local',
-    specs: ['./test/specs/**/*.js'],
+
+    specs: [
+        './test/specs/**/*.js'
+    ],
+
     maxInstances: 1,
+
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: [
+                '--headless=new',
+                '--no-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu',
+                '--window-size=1920,1080'
+            ]
+        }
     }],
+
     logLevel: 'info',
+
+    bail: 0,
+
+    baseUrl: 'https://testautomationpractice.blogspot.com/',
+
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+
     framework: 'mocha',
-    reporters: ['spec', ['allure', {
-        outputDir: 'allure-results',
-        disableWebdriverStepsReporting: true,
-        disableWebdriverScreenshotsReporting: false,
-    }]],
+
+    reporters: [
+        'spec',
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: false,
+        }]
+    ],
+
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
     },
+
+    /**
+     * Hooks (very useful for debugging in CI)
+     */
+    afterTest: async function (test, context, { error }) {
+        if (error) {
+            await browser.takeScreenshot();
+        }
+    }
 };
